aaa4511d1b5dbc2e953868012e1e0e194160dfd4
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2024-12-11 07:36:22 +0700

    📝 add Header

--- lib/common/helpers/is_dark_mode.dart
+++ lib/common/helpers/is_dark_mode.dart
@@ -1,3 +1,9 @@
+/*
+ * @ Author: Flutter Journey 🎯 <flutterjourney.org@gmail.com>
+ * @ Created: 2024-12-11 07:31:40
+ * @ Message: You look very hardworking 👨‍💻. Keep focusing on your goals. 🌤️
+ */
+
 import 'package:flutter/material.dart';
 
 extension DarkMode on BuildContext {
